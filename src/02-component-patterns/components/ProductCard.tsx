1e99cd728bee58ffa8bf90e99d423d82ba8342c3
Noel Perez <erick.perez1927@gmail.com>
2022-03-18 10:26:04 -0600

    parte de la seccion control props

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -2,20 +2,33 @@ import styles from '../styles/styles.module.css';
 
 import { useProduct } from '../hooks/useProduct';
 import { createContext, ReactElement, useContext } from 'react';
-import { Product, ProductContextProps } from '../interfaces/interfacesProducts';
+import {
+  onChangeArgs,
+  Product,
+  ProductContextProps,
+} from '../interfaces/interfacesProducts';
 
 export interface Props {
   product: Product;
   children?: ReactElement | ReactElement[];
   className?: string;
   style?: React.CSSProperties;
+  onChange?: (args: onChangeArgs) => void;
+  value?: number;
 }
 
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 
-export const ProductCard = ({ product, children, className, style }: Props) => {
-  const { counter, increaseBy } = useProduct();
+export const ProductCard = ({
+  product,
+  children,
+  className,
+  style,
+  onChange,
+  value,
+}: Props) => {
+  const { counter, increaseBy } = useProduct({ onChange, product, value });
   return (
     <Provider value={{ counter, increaseBy, product }}>
       <div className={`${styles.productCard} ${className}`} style={style}>
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,46 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   ProductButtons,
   ProductCard,
   ProductImage,
   ProductTitle,
 } from '../components';
+import { Product } from '../interfaces/interfacesProducts';
 import '../styles/styles-custom.css';
 
-const product = {
+const product1 = {
   id: 'ADSDS',
   title: 'Coffe - Mug m1',
   img: './coffee-mug.png',
 };
+const product2 = {
+  id: 'Producto 2',
+  title: 'Coffe - Mug m2',
+  img: './coffee-mug2.png',
+};
+
+const products: Product[] = [product1, product2];
+
+interface ProductInCart extends Product {
+  count: number;
+}
 
 export const ShoppingPage = () => {
+  /**
+   * <> generico
+   * {} objeto
+   * [key:string] sintaxis para especificar x cantidad
+   * {[key:string]}: ProductInCart especificamos el tipo del objeto
+   * asi se ve el objeto
+   * {'1': ...product1, count: 10}
+   */
+  const [shoppingCart, setShoppingCart] = useState<{
+    [key: string]: ProductInCart;
+  }>({});
+
+  const onProductCountChange = ({
+    count,
+    product,
+  }: {
+    count: number;
+    product: Product;
+  }) => {
+    setShoppingCart((oldShoppingCart) => {
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+        return rest;
+      }
+      return {
+        ...oldShoppingCart,
+        [product.id]: { ...product, count },
+      };
+    });
+  };
+
   return (
     <div>
       <h1>Shopping Store</h1>
       <hr />
       <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-        <ProductCard product={product} className='bg-dark'>
-          <ProductCard.Image className='custom-images' />
-          <ProductCard.Title className='text-white' />
-          <ProductCard.Buttons className='custom-buttons' />
-        </ProductCard>
-
-        <ProductCard product={product} className='bg-dark'>
-          <ProductImage className='custom-images' />
-          <ProductTitle className='text-white' />
-          <ProductButtons className='custom-buttons' />
-        </ProductCard>
-
-        <ProductCard
-          product={product}
-          style={{
-            backgroundColor: '#e44',
-          }}
-        >
-          <ProductImage />
-          <ProductTitle />
-          <ProductButtons />
-        </ProductCard>
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            className='bg-dark'
+            onChange={onProductCountChange}
+            value={shoppingCart[product.id]?.count || 0}
+          >
+            <ProductImage className='custom-images' />
+            <ProductTitle className='text-white' />
+            <ProductButtons className='custom-buttons' />
+          </ProductCard>
+        ))}
+      </div>
+      <div className='shopping-card'>
+        {Object.entries(shoppingCart).map(([key, product]) => (
+          <ProductCard
+            key={key}
+            product={product}
+            className='bg-dark'
+            style={{ width: '100px' }}
+            onChange={onProductCountChange}
+            value={product.count}
+          >
+            <ProductImage className='custom-images' />
+            <ProductTitle className='text-white' />
+            <ProductButtons className='custom-buttons' />
+          </ProductCard>
+        ))}
       </div>
     </div>
   );
